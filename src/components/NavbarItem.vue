56c11b7ce613cb48bc2093f8d130bf0bc806b9c2
Vehcamut <vehcamut@gmail.com>
2022-09-11 19:37:13 +0400

    made transitions with vue-router

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -2,8 +2,12 @@
   <v-container fluid>
     <v-app-bar color="orange" dark>
       <v-spacer />
-      <v-btn outlined color="white" class="ml-3">Главная</v-btn>
-      <v-btn outlined color="white" class="ml-3">Фотографии</v-btn>
+      <v-btn outlined color="white" class="ml-3">
+        <router-link to="/">Главная</router-link>
+      </v-btn>
+      <v-btn outlined color="white" class="ml-3">
+        <router-link to="/photo">Фотографии</router-link>
+      </v-btn>
     </v-app-bar>
   </v-container>
 </template>
