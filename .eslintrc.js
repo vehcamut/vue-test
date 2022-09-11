ecb2e10e8c8b0a8464596d7ef4ea7fa8bb7664db
Vehcamut <vehcamut@gmail.com>
2022-09-11 14:13:11 +0400

    problem solved: missing file extension "ts"

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,16 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
