997d73e9c15fdd3e91c23a3f252384a50044b303
Vehcamut <vehcamut@gmail.com>
2022-09-12 15:32:25 +0400

    made element for photo

--- src/components/photo/PhotoItem.vue
+++ src/components/photo/PhotoItem.vue
@@ -9,13 +9,25 @@ export default defineComponent({
       required: true,
     },
   },
+  methods: {
+    openPhoto() {
+      this.$emit('openPhoto', this.photo);
+    },
+  },
 });
 </script>
 
 <template>
-  <div>
-    {{photo.id + ' ' + photo.title}}
-  </div>
+  <v-col cols="4">
+    <v-card @click="openPhoto">
+      <v-card-title primary-title>
+        {{photo.title}}
+      </v-card-title>
+      <v-card-text>
+        <v-img width="200" height="200" :src="`${photo.url}`"></v-img>
+      </v-card-text>
+    </v-card>
+  </v-col>
 </template>
 
 <style lang="">
