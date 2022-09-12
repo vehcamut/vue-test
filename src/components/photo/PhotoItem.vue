7de4c0f6c997c07a8ef817549130a250243b3e2a
Vehcamut <vehcamut@gmail.com>
2022-09-12 12:26:20 +0400

    added PhotoItem element

---
+++ src/components/photo/PhotoItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IPhoto } from '../../interfaces';
+
+export default defineComponent({
+  props: {
+    photo: {
+      type: Object as () => IPhoto,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    {{photo.id + ' ' + photo.title}}
+  </div>
+</template>
+
+<style lang="">
+
+</style>
